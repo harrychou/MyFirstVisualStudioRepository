4a40aeee0c6eb07c7445b76b7b29f1341e8cb7d8
Harry Chou <harrychou@gmail.com>
2009-10-22 00:32:02 -0400

    second commit

--- PlayWithReflection/Program.cs
+++ PlayWithReflection/Program.cs
@@ -10,10 +10,43 @@ namespace PlayWithReflection
             var types = Assembly.GetExecutingAssembly().GetTypes();
             foreach (var type in types)
             {
+                Console.WriteLine("types ............... ");
                 Console.WriteLine("type name: " + type.FullName);
+                var constructors = type.GetConstructors();
+                foreach (var constructor in constructors)
+                {
+                    Console.WriteLine("constructors ............... ");
+
+                    var parameters = constructor.GetParameters();
+
+                    Console.WriteLine("parameters ............... ");
+                    foreach (var parameter in parameters)
+                    {
+                        Console.WriteLine(parameter.Name);
+                    }
+                }
             }
 
             Console.ReadKey();
         }
+    }
+
+    public static class Extension
+    {
+    }
+
+    public class Employer
+    {
+        int _i;
+
+        public Employer(int i) {
+            _i = i;
+        }
+
+        public string Name { get; set; }
+    }
+
+    public class Employee
+    {
     }
 }
