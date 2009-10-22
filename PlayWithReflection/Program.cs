10b07682140511256b8fbd877df4f0f6aa8e38dc
Harry Chou <harrychou@gmail.com>
2009-10-22 00:23:18 -0400

    first commit

---
+++ PlayWithReflection/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Reflection;
+
+namespace PlayWithReflection
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var types = Assembly.GetExecutingAssembly().GetTypes();
+            foreach (var type in types)
+            {
+                Console.WriteLine("type name: " + type.FullName);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
